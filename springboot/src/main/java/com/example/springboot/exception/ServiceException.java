8d5ea0894d840014724f57c46de5a10ae58357d8
wbh <1262276637@qq.com>
2025-02-11 06:37:55 +0800

    提交JWT Token功能。 包括写个拦截器、自定义拦截规则、如何生成Token

--- springboot/src/main/java/com/example/springboot/exception/ServiceException.java
+++ springboot/src/main/java/com/example/springboot/exception/ServiceException.java
@@ -1,13 +1,24 @@
 package com.example.springboot.exception;
 
+import lombok.Getter;
+
 /**
  * 功能：
  * 作者：王柄皓
  * 日期：2025/2/5 19:51
  */
+@Getter
 public class ServiceException extends RuntimeException{
+
+    private final String code;
 
     public ServiceException(String msg){
         super(msg);
+        this.code = "500";
+    }
+
+    public ServiceException(String code, String msg){
+        super(msg);
+        this.code = code;
     }
 }
